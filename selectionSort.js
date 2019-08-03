e11a6cfe13f7b41ffe6fc0dc4b0ad74a3a335641
asalia-1 <asalia@wesleyan.edu>
2019-08-02 21:10:22 -0400

    Updating files

---
+++ selectionSort.js
@@ -0,0 +1,39 @@
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+var indexOfMinimum = function(array, startIndex) {
+
+    var minValue = array[startIndex];
+    var minIndex = startIndex;
+
+    for(var i = minIndex + 1; i < array.length; i++) {
+        if(array[i] < minValue) {
+            minIndex = i;
+            minValue = array[i];
+        }
+    } 
+    return minIndex;
+}; 
+
+var selectionSort = function(array) {
+    for (var i = 0; i < array.length; i++){
+        
+        var result = indexOfMinimum(array, i);
+        swap(array, i, result);
+    }
+	return array
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+
+selectionSort(array);
+
+println("Array after sorting:  " + array);
+Program.assertEqual(array, [7, 9, 11, 22, 42, 88, 99]);
+
+var array_1 = [6, 5, 7, 2, 1, 3, 4]; 
+selectionSort(array_1);
+Program.assertEqual(array_1, [1, 2, 3, 4, 5, 6, 7]);
